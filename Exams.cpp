22cda37641828f9846c2622f673c110a2603aad6
milessmith23 <miles.smith001@albright.edu>
2025-02-03 12:28:43 -0500

    Initial Commit

---
+++ Exams.cpp
@@ -0,0 +1,40 @@
+// Miles Smith
+// 1/29/25
+// This file asks what your name is and to put in your test scores and it will find the average of your three test score.
+// Exams.cpp
+
+#include <iostream>
+#include <iomanip> 
+
+using namespace std;
+
+int main() {
+
+    string name;
+    int score1, score2, score3;
+    
+    cout << "What is your name ? ";
+    getline(cin, name); 
+    
+    
+    cout << "Enter your first exam: ";
+    cin >> score1;
+    cout << "Enter your second exam: ";
+    cin >>score2;
+    cout << "Enter your third exam: ";
+    cin >> score3;
+   
+    double average = (score1 + score2 + score3) / 3.0; 
+    
+    
+    cout << "" <<endl;
+    cout << "Hello," << name << endl;
+    cout << "Your 3 exams scores are: " << score1 << ", " << score2 << ", " << score3 << endl;
+    
+   cout << "Your average exam score is " << fixed << setprecision(2) << average << " points." << endl;
+
+    
+
+    return 0;
+}
+
---
+++ gasmileage.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main() {   
+    
+    const double KM_PER_MILE = 1.60934;      
+    const double LITERS_PER_GALLON = 3.78541; 
+    
+    
+    std::string desc;
+    int miles;
+    float gallons;      
+    float mpg;
+    float liters;
+    float km_per_liter;
+    float kilometers;
+
+    
+    std::cout << "Enter a description of your car: ";
+    std::getline(std::cin, desc);
+
+    std::cout << "How many miles have you traveled? ";
+    std::cin >> miles;
+
+    std::cout << "How many gallons of gas did you use? ";
+    std::cin >> gallons;
+
+    kilometers = miles * KM_PER_MILE;
+    liters = gallons * LITERS_PER_GALLON;
+    mpg = miles / gallons;
+    km_per_liter = kilometers / liters;       
+    
+   std::cout<<"\n";
+    std::cout<<"Fuel efficiency of your " + desc + ":"<<std::endl;
+    std::cout<<"You drove " <<miles<< " miles and used " <<gallons<< " gallons."<<std::endl;
+    std::cout<<"Your fuel efficiency is " << mpg << " miles per gallon."<<std::endl; 
+    
+
+    std::cout<<"\n";
+    std::cout<<"Fuel efficiency of your " + desc + ":"<<std::endl;
+    std::cout<<"You drove " << kilometers << " kilometers and used " << liters << " liters."<<std::endl; 
+    std::cout << "Your fuel efficiency is " << km_per_liter << " kilometers per liter.\n";
+    return 0;
+}
