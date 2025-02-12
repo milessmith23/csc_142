98a1bf5e8d20c821b1b91f794cee823fec96d430
milessmith23 <miles.smith001@albright.edu>
2025-02-12 12:24:14 -0500

    week 4

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+
+
+bool isEven(int num) {
+    return num % 2 == 0;
+}
+
+int main() {
+    std::srand(std::time(0)); 
+    int evenCount = 0, oddCount = 0;
+
+    
+    for (int i = 0; i < 1000; ++i) {
+        int num = std::rand() % 101; 
+        if (isEven(num)) evenCount++;
+        else oddCount++;
+    }
+
+   
+    std::cout << "Total numbers: 1000\n";
+    std::cout << "Even numbers: " << evenCount << "\n";
+    std::cout << "Odd numbers: " << oddCount << "\n";
+
+    return 0;
+}
